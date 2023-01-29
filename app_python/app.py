299141a75475cb6bf6023ecbc4e14cd0ca755908
Tasneem <tasneemsherif23@gmil.com>
2023-01-29 20:26:26 +0300

    finish lab12 with bonus

--- app_python/app.py
+++ app_python/app.py
@@ -2,24 +2,44 @@
 An Application to show Current time in Moscow
 """
 import logging
+import os
+import json
+
 from datetime import datetime
-
-from flask import Flask
+from flask import Flask, jsonify
 from waitress import serve
-
 import pytz
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 app = Flask(__name__)
-
+FILE_NAME = "./data/visits.json"
 
 @app.route('/')
 def index():
     """This is the entry point for specified root"""
+    visits = 0
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r") as f:
+            visits = json.load(f)
+    else:
+        os.makedirs(os.path.dirname(FILE_NAME), exist_ok=True)
+    with open(FILE_NAME, "w") as f:
+        json.dump(visits + 1, f)
+
     timezone = pytz.timezone("Europe/Moscow")
     now = datetime.now(timezone).strftime("%H:%M:%S - %d/%m/%Y")
     return f'<h1>{str(now)}</h1>'
+
+
+@app.route("/visits")
+def visit():
+    """Sends back the total number of visits to the root endpoint."""
+    visits = 0
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r") as f:
+            visits = json.load(f)
+    return jsonify(visits)
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
